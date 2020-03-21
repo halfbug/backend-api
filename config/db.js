0774f963570a9be32be26daa53714c0d8d4413b2
Sadaf <sadaf_cu@hotmail.com>
2020-03-21 14:25:53 +0500

    configure heroku

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  console.log("Mongo URI" , process.env.MONGO_URI)
+  // console.log("Mongo URI" , process.env.MONGO_URI)
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
