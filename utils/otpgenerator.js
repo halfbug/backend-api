ce4d5fd5a14a9e78e88eb381bfde144b2338e851
Sadaf <sadaf_cu@hotmail.com>
2020-05-17 15:55:51 +0500

    generate 4 numbers as OTP

--- utils/otpgenerator.js
+++ utils/otpgenerator.js
@@ -1,7 +1,8 @@
 
 
 const OTPGenerator = ()=>{
-return Math.floor(100000 + Math.random() * 900000)
+return Math.floor(1000 + Math.random() * 9000) // for four
+//Math.floor(100000 + Math.random() * 900000) // for six
 } 
   
   module.exports = OTPGenerator;
