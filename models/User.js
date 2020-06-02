fc5f9e1f1f37db7d6db2e55137a1c7557e6c0612
Sadaf <sadaf_cu@hotmail.com>
2020-06-02 14:40:33 +0500

    remove email from mandatory fields

--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
   
   email: {
     type: String,
-    required: [true, 'Please add an email'],
+    // required: [true, 'Please add an email'],
     unique:[ true, 'User already present'],
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
@@ -33,7 +33,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, 'Please add a password'],
+    // required: [true, 'Please add a password'],
     minlength: 6,
     select: false
   },
