1e70ba18c87ebf7a9d1b599eb0680b8559655b74
Sadaf <sadaf_cu@hotmail.com>
2020-09-22 11:47:34 +0500

    upload the image upload path for the products

--- controllers/product.js
+++ controllers/product.js
@@ -83,7 +83,7 @@ console.log(req.files)
        // Create custom filename
        file.name = `${product._id}_${slugify(path.parse(file.name).name, { lower: true })}${path.parse(file.name).ext}`;
 
-       file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+       file.mv(`${process.env.PRODUCT_UPLOD_PATH}/${file.name}`, async err => {
              if (err) {
                console.error(err);
                return next(new ErrorResponse(`Problem with Document upload`, 500));
