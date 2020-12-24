79c3136e7f29f6d28fad5712336bd78daf48dc41
ahmerdevopsinertia <58416271+ahmerdevopsinertia@users.noreply.github.com>
2020-12-25 01:36:10 +0500

    fixed the issue

--- controllers/wallet.js
+++ controllers/wallet.js
@@ -2,23 +2,33 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 const Wallet = require('../models/Wallet');
-
+const ObjectId = require('mongoose').Types.ObjectId; 
 
 // @desc      Get wallet details of user in array
 // @route     GET /api/v1/user/wallet
 // @access    Private/Authorized User
 exports.getUserWallet = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user._id);
-    if (user.isKycDocVerified) {
-        const userWallet = await Wallet.find({ 'userId': user._id });
-        res.status(200).json({
-            success: true,
-            data: userWallet
-        });
+    console.log(`request ${req.user._id}`)
+    const userData = await User.findOne({'_id': ObjectId(req.user._id)});
+    console.log(userData);
+    if (userData.isKycDocVerified) {
+        const userWallet = await Wallet.findOne({ 'userId': ObjectId(userData._id) });
+        console.log(`Date ${userWallet}`);
+        if(!userWallet) {
+            res.status(200).json({
+                success: 'User does not have the Wallet yet',
+                data: userWallet
+            });
+        } else {
+            res.status(200).json({
+                success: 'Successfully fetched the User Wallet',
+                data: userWallet
+            });
+        }
     }
     else {
         // 'User not authorized to access the wallets details. Need to attach KYC documents'
-        res.status(401).json({
+        res.status(200).json({
             success: false,
             data: []
         });
@@ -31,7 +41,7 @@ exports.getUserWallet = asyncHandler(async (req, res, next) => {
 // @route     POST /api/v1/user/wallet
 // @access    Private/Authorized User
 exports.createUserWallet = asyncHandler(async (req, res, next) => {
-    console.log(req.body);
+    console.log(`${req.body}`);
     const userWallet = await Wallet.create(req.body);
 
     res.status(201).json({
