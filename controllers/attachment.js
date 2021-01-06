2f3ad3361490766e282dba8b54e8a9a24d54eb83
ahmerdevopsinertia <58416271+ahmerdevopsinertia@users.noreply.github.com>
2021-01-06 15:02:27 +0500

    added API to upload the multiple kyc docs

--- controllers/attachment.js
+++ controllers/attachment.js
@@ -9,16 +9,13 @@ const User = require('../models/User');
 
 // @desc      upload attachment
 // @route     POST /api/v1/attachment/upload
-// @access    Public
+// @access    Protected
 exports.upload = asyncHandler(async (req, res, next) => {
   if (req.files) {
-    console.log('file is uploaded');
+    console.log('file is attached');
     let record = {};
     Object.keys(req.files).forEach((key, i) => {
-      const file = req.files[key]
-      console.log("----------------")
-      console.log(key)
-      console.log(file)
+      const file = req.files[key];
       if (path.parse(file.name).ext > process.env.MAX_FILE_UPLOAD) {
         return next(
           new ErrorResponse(
@@ -35,7 +32,6 @@ exports.upload = asyncHandler(async (req, res, next) => {
           )
         );
       }
-      console.log(path.parse(file.name).name)
       // Create custom filename
       const extension = path.parse(file.name).ext;
       const customName = `${Date.now()}_${path.parse(file.name).name.split('.')[0]}`;
@@ -69,6 +65,10 @@ exports.upload = asyncHandler(async (req, res, next) => {
   }
   else {
     console.log('file is not uploaded')
+    res.status(400).json({
+      success: true,
+      data: "KYC-Document(s) are not attached. Please attached them first"
+    });
   }
   res.status(200).json({
     success: true,
@@ -76,9 +76,107 @@ exports.upload = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      upload kyc attachments
+// @route     POST /api/v1/attachment/upload/kyc
+// @access    Protected
+exports.uploadKycDocs = asyncHandler(async (req, res, next) => {
+
+  if (req.files) {
+    console.log(`file is attached ${JSON.stringify(req.files)}`);
+    let record = {};
+    if (Array.isArray(req.files.file)) {
+      req.files.file.forEach((key) => {
+        if (path.parse(key.name).ext > process.env.MAX_FILE_UPLOAD) {
+          return next(
+            new ErrorResponse(
+              `Please upload file less than ${process.env.MAX_FILE_UPLOAD}`,
+              400
+            )
+          );
+        }
+
+        // Create custom filename
+        const extension = path.parse(key.name).ext;
+        const customName = `${Date.now()}_${path.parse(key.name).name.split('.')[0]}`;
+        key.name = `${slugify(path.parse(key.name).name, { lower: true })}${extension}`;
+        key.mv(`.${process.env.ATTACHMENT_UPLOD_PATH}/${customName}${extension}`, async err => {
+          if (err) {
+            console.error(err);
+            return next(new ErrorResponse(`Problem with uploaded Document`, 500));
+          }
+        });
+        record.userId = req.user._id;
+        record.originalName = key.name.split('.')[0];
+        record.customName = customName;
+        record.extension = extension;
+        record.mimeType = key.mimetype;
+        record.size = key.size;
+        record.relativePath = process.env.ATTACHMENT_UPLOD_PATH;
+        record.relatedTo = attachmentsRelatedTo.kycDoc[0];
+
+        Attachment.create(record);
+
+      });
+    }
+    else {
+      Object.keys(req.files).forEach((key, i) => {
+        const file = req.files[key]
+        if (path.parse(file.name).ext > process.env.MAX_FILE_UPLOAD) {
+          return next(
+            new ErrorResponse(
+              `Please upload file less than ${process.env.MAX_FILE_UPLOAD}`,
+              400
+            )
+          );
+        }
+        // Create custom filename
+        const extension = path.parse(file.name).ext;
+        const customName = `${Date.now()}_${path.parse(file.name).name.split('.')[0]}`;
+        file.name = `${slugify(path.parse(file.name).name, { lower: true })}${extension}`;
+        file.mv(`.${process.env.ATTACHMENT_UPLOD_PATH}/${customName}${extension}`, async err => {
+          if (err) {
+            console.error(err);
+            return next(new ErrorResponse(`Problem with Document upload`, 500));
+          }
+        });
+        record.userId = req.user._id;
+        record.originalName = file.name.split('.')[0];
+        record.customName = customName;
+        record.extension = extension;
+        record.mimeType = file.mimetype;
+        record.size = file.size;
+        record.relativePath = process.env.ATTACHMENT_UPLOD_PATH;
+        record.relatedTo = attachmentsRelatedTo.kycDoc[0];
+
+        console.log('call the db model to insert the data now');
+
+        Attachment.create(record);
+
+        console.log(`attachment record is inserted`);
+      });
+    }
+
+    await User.findByIdAndUpdate(req.user._id, { isKycDocSubmitted: true }, {
+      new: false,
+      runValidators: true
+    });
+  }
+  else {
+    console.log('file is not attached')
+    res.status(400).json({
+      success: false,
+      data: "KYC-Document(s) are not attached. Please attached them first"
+    });
+  }
+  res.status(200).json({
+    success: true,
+    data: "KYC-Document(s) are uploaded successfully. Will notify you once it is verified"
+  });
+});
+
 // @desc      view attachment
 // @route     GET /api/v1/view/download
-// @access    Public
+// @access    Protected
 exports.view = asyncHandler(async (req, res, next) => {
 
   let attachment = await Attachment.findById(req.params.id);
